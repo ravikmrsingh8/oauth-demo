2179de37c9f1a528d246128baa26142220fe87e1
Ravi Kumar Singh <ravi.kmr.singh8@gmail.com>
2024-03-28 10:49:02 +0530

    Added timestamp validator for jwt

--- product-service/src/main/java/com/example/oauth/config/JwtConfigs.java
+++ product-service/src/main/java/com/example/oauth/config/JwtConfigs.java
@@ -9,7 +9,10 @@ import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtIssuerValidator;
+import org.springframework.security.oauth2.jwt.JwtTimestampValidator;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import java.time.Duration;
 
 
 @Configuration
@@ -22,6 +25,7 @@ public class JwtConfigs {
     JwtDecoder jwtDecoder() {
         NimbusJwtDecoder decoder = NimbusJwtDecoder.withJwkSetUri(config.getJwkSetUri()).build();
         OAuth2TokenValidator<Jwt> jwtValidator = new DelegatingOAuth2TokenValidator<>(
+                new JwtTimestampValidator(Duration.ofSeconds(60)),
                 new JwtIssuerValidator(config.getIssuerUri()),
                 new AudienceValidator(config.getAudience())
         );
